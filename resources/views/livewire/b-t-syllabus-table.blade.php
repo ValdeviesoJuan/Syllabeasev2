bb6e47135566bae04a6a16fd62f06e3c5c31da90
Gelightful <gelopagutayao@gmail.com>
2025-07-09 19:43:55 +0800

    commit
    B Teacher (Syllabus) Page,B Teacher (TOS) Page and B Teacher (Home) Page "Dropdown filter width issue"

--- resources/views/livewire/b-t-syllabus-table.blade.php
+++ resources/views/livewire/b-t-syllabus-table.blade.php
@@ -1,25 +1,40 @@
 <div class=" text-left">
-    <div class="">
-        <input wire:model.live="search" type="text" class="border focus:outline-none focus:border-blue mb-6 border-black w-[20%] rounded p-1" placeholder="Search...">
-        <button wire:click="applyFilters"><svg width="34px" class="rounded-lg p-[3px] -mb-3" height="34px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#2468d2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-            </svg>
+    <div class="flex flex-wrap items-center gap-3 mb-4">
+        <div class="relative w-[18%]">
+            <input
+                wire:model.live="search"
+                wire:keydown.enter="applyFilters"
+                type="text"
+                class="border focus:outline-none focus:border-blue border-black w-full rounded p-1 pr-10"
+                placeholder="Search..."
+            >
+            <button
+                type="button"
+                wire:click="applyFilters"
+                class="absolute inset-y-0 right-0 flex items-center px-2"
+                tabindex="-1"
+            >
+                <svg width="24px" class="rounded-lg p-[2px]" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#2468d2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </button>
+        </div>
         </button>
-        <select wire:model="filters.course_year_level" class="border cursor-pointer focus:outline-none focus:border-blue rounded p-1 w-[12%] ml-6" placeholder="Year level">
-            <option value="" class="">Year level (All)</option>
+        <select wire:model="filters.course_year_level" class="border cursor-pointer focus:outline-none focus:border-blue rounded p-1 w-[16%]" placeholder="Year level">
+            <option value="">Year level (All)</option>
             <option value="1st Year">1st Year</option>
             <option value="2nd Year">2nd Year</option>
             <option value="3rd Year">3rd Year</option>
             <option value="4th Year">4th Year</option>
             <option value="5th Year">5th Year</option>
         </select>
-        <select wire:model="filters.course_semester" class="border focus:outline-none focus:border-blue cursor-pointer rounded p-1 w-[12%] ml-6" placeholder="Semester">
+        <select wire:model="filters.course_semester" class="border focus:outline-none focus:border-blue cursor-pointer rounded p-1 w-[16%]" placeholder="Semester">
             <option value="">Semester (All)</option>
             <option value="1st Semester">1st Semester</option>
             <option value="2nd Semester">2nd Semester</option>
             <option value="Mid Year">Mid Year</option>
         </select>
-        <select wire:model="filters.bg_school_year" class="border focus:outline-none focus:border-blue cursor-pointer rounded p-1 w-[12%] ml-6" placeholder="School Year">
+        <select wire:model="filters.bg_school_year" class="border focus:outline-none focus:border-blue cursor-pointer rounded p-1 w-[18%]" placeholder="School Year">
             <option value="">School Year (All)</option>
             <option value="2023-2024">2023-2024</option>
             <option value="2024-2025">2024-2025</option>
@@ -29,8 +44,7 @@
             <option value="2028-2029">2028-2029</option>
             <option value="2029-2030">2029-2030</option>
         </select>
-
-        <select wire:model="filters.status" class="border focus:outline-none focus:border-blue cursor-pointer rounded p-1 w-[12%] ml-6" placeholder="Semester">
+        <select wire:model="filters.status" class="border focus:outline-none focus:border-blue cursor-pointer rounded p-1 w-[15%]" placeholder="Status">
             <option value="">Status (All)</option>
             <option value="Pending">Pending</option>
             <option value="Approved by Chair">Approved by Chair</option>
@@ -38,7 +52,7 @@
             <option value="Approved by Dean">Approved by Dean</option>
             <option value="Returned by Dean">Returned by Dean</option>
         </select>
-        <button wire:click="applyFilters" class="bg-blue5 hover:bg-blue focus:outline-none focus:border-blue cursor-pointer rounded text-white p-[4px] -mb-3">Apply Filters</button>
+        <button wire:click="applyFilters" class="bg-blue5 hover:bg-blue focus:outline-none focus:border-blue cursor-pointer rounded text-white p-[4px]">Apply Filters</button>
     </div>
     <table class=" w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="rounded text-xs text-white uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
