2420289bd0ace7912e4675c29a72dc7c0ac4cb16
Gelightful <gelopagutayao@gmail.com>
2025-07-16 15:11:26 +0800

    commit
    Auditor (Syllabus) Page & Auditor (TOS) Page : "Dropdown filter width issue" ,"Dropdown filter too big" Done

--- resources/views/livewire/auditor-syllabus-table.blade.php
+++ resources/views/livewire/auditor-syllabus-table.blade.php
@@ -1,20 +1,47 @@
-<div class=" text-left">
-    <div class="flex justify-between mb-4">
-        <select wire:model="filters.course_year_level" class="border cursor-pointer focus:outline-none focus:border-blue rounded-lg p-1 w-[15%] ml-" placeholder="Year level">
-            <option value="" class="">Year level (All)</option>
+<div class="text-left">
+    <div class="flex flex-wrap items-center gap-3 mb-4">
+        <!--Search Input  -->
+        <div class="relative w-[19%]">
+            <input type="text" wire:model.live="search" id="table-search"
+                class="border focus:outline-none focus:border-blue border-black w-full rounded p-1 pr-10"
+                placeholder="Search...">
+            <button type="button" wire:click="applyFilters"
+                class="absolute inset-y-0 right-0 flex items-center px-2" tabindex="-1">
+                <svg width="24px" class="rounded-lg p-[2px]" height="24px" viewBox="0 0 24 24" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
+                        stroke="#2468d2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </button>
+        </div>
+
+        <!-- Year Level -->
+        <select wire:model="filters.course_year_level"
+            class="border cursor-pointer focus:outline-none focus:border-blue rounded p-1 w-[15%]"
+            placeholder="Year level">
+            <option value="">Year level (All)</option>
             <option value="1st Year">1st Year</option>
             <option value="2nd Year">2nd Year</option>
             <option value="3rd Year">3rd Year</option>
             <option value="4th Year">4th Year</option>
             <option value="5th Year">5th Year</option>
         </select>
-        <select wire:model="filters.course_semester" class="border focus:outline-none focus:border-blue cursor-pointer rounded-lg p-1 w-[15%] ml-" placeholder="Semester">
+
+        <!-- Semester -->
+        <select wire:model="filters.course_semester"
+            class="border focus:outline-none focus:border-blue cursor-pointer rounded p-1 w-[15%]"
+            placeholder="Semester">
             <option value="">Semester (All)</option>
             <option value="1st Semester">1st Semester</option>
             <option value="2nd Semester">2nd Semester</option>
             <option value="Mid Year">Mid Year</option>
         </select>
-        <select wire:model="filters.bg_school_year" class="border focus:outline-none focus:border-blue cursor-pointer rounded-lg p-1 w-[15%] ml-" placeholder="School Year">
+
+        <!-- School Year -->
+        <select wire:model="filters.bg_school_year"
+            class="border focus:outline-none focus:border-blue cursor-pointer rounded p-1 w-[17%]"
+            placeholder="School Year">
             <option value="">School Year (All)</option>
             <option value="2023-2024">2023-2024</option>
             <option value="2024-2025">2024-2025</option>
@@ -25,52 +52,36 @@
             <option value="2029-2030">2029-2030</option>
         </select>
 
-        <select wire:model="filters.department_id" class="border focus:outline-none focus:border-blue cursor-pointer rounded-lg p-1 w-[14%] ml-" placeholder="Semester">
+        <!-- Department -->
+        <select wire:model="filters.department_id"
+            class="border focus:outline-none focus:border-blue cursor-pointer rounded p-1 w-[16%]"
+            placeholder="Department">
             <option value="">Department (All)</option>
             @foreach ($departments as $department)
                 <option value="{{ $department->department_id }}">{{ $department->department_code }}</option>
             @endforeach
         </select>
-        <button wire:click="applyFilters" class="bg-blue5 focus:outline-none focus:border-blue cursor-pointer rounded-lg text-white p-[4px] px-4">Apply Filters</button>
-        <div class="relative">
-            <div class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
-                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
-                </svg>
-            </div>
-            <input type="text" wire:model.live="search" id="table-search" class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...">
-        </div>
+
+        <!-- Apply Button -->
+        <button wire:click="applyFilters"
+            class="bg-blue5 hover:bg-blue focus:outline-none focus:border-blue cursor-pointer rounded text-white p-[4px] px-4">
+            Apply Filters
+        </button>
     </div>
-    <table class=" w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+
+    <!-- Table content -->
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="rounded text-xs text-white uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" class="bg-blue5 rounded-tl-lg px-6 py-3">
-                    Course Code
-                </th>
-                <th scope="col" class="bg-blue5 px-6 py-3">
-                    Course Title
-                </th>
-                <th scope="col" class="bg-blue5 px-6 py-3">
-                    School Year
-                </th>
-                <th scope="col" class="bg-blue5 px-6 py-3">
-                    Semester
-                </th>
-                <th scope="col" class="bg-blue5 px-6 py-3">
-                    Date Submitted At
-                </th>
-                <th scope="col" class="bg-blue5 px-6 py-3">
-                    Date Approved At
-                </th>
-                <th scope="col" class="bg-blue5 px-6 py-3">
-                    Version
-                </th>
-                <th scope="col" class="bg-blue5 px-6 py-3">
-                    Status
-                </th>
-                <th scope="col" class="bg-blue5 rounded-tr-lg px-6 py-3">
-                    Action
-                </th>
+                <th scope="col" class="bg-blue5 rounded-tl-lg px-6 py-3">Course Code</th>
+                <th scope="col" class="bg-blue5 px-6 py-3">Course Title</th>
+                <th scope="col" class="bg-blue5 px-6 py-3">School Year</th>
+                <th scope="col" class="bg-blue5 px-6 py-3">Semester</th>
+                <th scope="col" class="bg-blue5 px-6 py-3">Date Submitted At</th>
+                <th scope="col" class="bg-blue5 px-6 py-3">Date Approved At</th>
+                <th scope="col" class="bg-blue5 px-6 py-3">Version</th>
+                <th scope="col" class="bg-blue5 px-6 py-3">Status</th>
+                <th scope="col" class="bg-blue5 rounded-tr-lg px-6 py-3">Action</th>
             </tr>
         </thead>
         <tbody>
@@ -79,36 +90,24 @@
                 <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     {{ $syllabus->course_code }}
                 </td>
-                <td class="px-6 py-4">
-                    {{ $syllabus->course_title }}
-                </td>
-                <td class="px-6 py-4">
-                    {{ $syllabus->bg_school_year }}
-                </td>
-                <td class="px-6 py-4">
-                    {{$syllabus->course_semester}}
-                </td>
-                <td class="px-6 py-4">
-                    {{$syllabus->chair_submitted_at}}
-                </td>
-                <td class="px-6 py-4">
-                    {{$syllabus->dean_approved_at}}
-                </td>
-                <td class="px-6 py-4">
-                    Version {{$syllabus->version}}
-                </td>
+                <td class="px-6 py-4">{{ $syllabus->course_title }}</td>
+                <td class="px-6 py-4">{{ $syllabus->bg_school_year }}</td>
+                <td class="px-6 py-4">{{ $syllabus->course_semester }}</td>
+                <td class="px-6 py-4">{{ $syllabus->chair_submitted_at }}</td>
+                <td class="px-6 py-4">{{ $syllabus->dean_approved_at }}</td>
+                <td class="px-6 py-4">Version {{ $syllabus->version }}</td>
                 <td class="px-6 py-4">
                     <button class="
                     {{ $syllabus->status === 'Pending' ? 'w-[80%] bg-amber-100 text-amber-500 border-2 border-amber-300 rounded-lg' : '' }}
-                    {{ $syllabus->status === 'Approved by Chair' ? 'w-[80%]  bg-emerald-200 text-emerald-600 border-2 border-emerald-400 rounded-lg' : '' }}
-                    {{ $syllabus->status === 'Returned by Chair' ? 'w-[80%] bg-rose-300 text-rose-600 border-2 border-rose-500 rounded-lg' : ' ' }}
-                    {{ $syllabus->status === 'Approved by Dean' ? 'w-[80%]  bg-emerald-200 text-emerald-600 border-2 border-emerald-400 rounded-lg' : '' }}
-                    {{ $syllabus->status === 'Returned by Dean' ? 'w-[80%] bg-rose-300 text-rose-600 border-2 border-rose-500 rounded-lg' : ' ' }}">
-                        {{$syllabus->status}}
+                    {{ $syllabus->status === 'Approved by Chair' ? 'w-[80%] bg-emerald-200 text-emerald-600 border-2 border-emerald-400 rounded-lg' : '' }}
+                    {{ $syllabus->status === 'Returned by Chair' ? 'w-[80%] bg-rose-300 text-rose-600 border-2 border-rose-500 rounded-lg' : '' }}
+                    {{ $syllabus->status === 'Approved by Dean' ? 'w-[80%] bg-emerald-200 text-emerald-600 border-2 border-emerald-400 rounded-lg' : '' }}
+                    {{ $syllabus->status === 'Returned by Dean' ? 'w-[80%] bg-rose-300 text-rose-600 border-2 border-rose-500 rounded-lg' : '' }}">
+                        {{ $syllabus->status }}
                     </button>
                 </td>
                 <td class="px-6 py-4 flex">
-                    <form class="" action="{{ route('auditor.commentSyllabus', $syllabus->syll_id) }}" method="GET">
+                    <form action="{{ route('auditor.commentSyllabus', $syllabus->syll_id) }}" method="GET">
                         @csrf
                         <div class="p-4">
                             <button class="hover:text-yellow hover:underline" type="submit">View</button>
@@ -120,5 +119,4 @@
         </tbody>
     </table>
     {{ $syllabi->links() }}
-
 </div>
--- resources/views/livewire/auditor-tos.blade.php
+++ resources/views/livewire/auditor-tos.blade.php
@@ -1,26 +1,46 @@
-<div class=" text-left">
-    <div class="">
-        <input wire:model.live="search" type="text" class="border focus:outline-none focus:border-blue mb-6 border-black w-[20%] rounded p-1" placeholder="Search...">
-        <button wire:click="applyFilters"><svg width="34px" class=" rounded p-[3px] -mb-3" height="34px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#2468d2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-            </svg>
-        </button>
-        <select wire:model="filters.course_year_level" class="border cursor-pointer focus:outline-none focus:border-blue rounded p-1 w-[12%] ml-6" placeholder="Year level">
-            <option value="" class="">Year level (All)</option>
+<div class="text-left">
+    <div class="flex flex-wrap items-center gap-3 mb-4">
+        <!-- Search Input-->
+        <div class="relative w-[15%]">
+            <input wire:model.live="search" type="text"
+                class="border focus:outline-none focus:border-blue border-black w-full rounded p-1 pr-10"
+                placeholder="Search...">
+            <button wire:click="applyFilters" type="button"
+                class="absolute inset-y-0 right-0 flex items-center px-2" tabindex="-1">
+                <svg width="24px" class="rounded-lg p-[2px]" height="24px" viewBox="0 0 24 24" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
+                        stroke="#2468d2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </button>
+        </div>
+
+        <!-- Filters -->
+        <select wire:model="filters.course_year_level"
+            class="border cursor-pointer focus:outline-none focus:border-blue rounded p-1 w-[16%]"
+            placeholder="Year level">
+            <option value="">Year level (All)</option>
             <option value="1st Year">1st Year</option>
             <option value="2nd Year">2nd Year</option>
             <option value="3rd Year">3rd Year</option>
             <option value="4th Year">4th Year</option>
             <option value="5th Year">5th Year</option>
         </select>
-        <select wire:model="filters.course_semester" class="border focus:outline-none focus:border-blue cursor-pointer rounded p-1 w-[12%] ml-6" placeholder="Semester">
-            <option value="">Semester (All)</option>
+
+        <select wire:model="filters.course_semester"
+            class="border focus:outline-none focus:border-blue cursor-pointer rounded p-1 w-[15%]"
+            placeholder="Semester">
+            <option value="">Semester(All)</option>
             <option value="1st Semester">1st Semester</option>
             <option value="2nd Semester">2nd Semester</option>
             <option value="Mid Year">Mid Year</option>
         </select>
-        <select wire:model="filters.bg_school_year" class="border focus:outline-none focus:border-blue cursor-pointer rounded p-1 w-[12%] ml-6" placeholder="School Year">
-            <option value="">School Year (All)</option>
+
+        <select wire:model="filters.bg_school_year"
+            class="border focus:outline-none focus:border-blue cursor-pointer rounded p-1 w-[17%]"
+            placeholder="School Year">
+            <option value="">School Year(All)</option>
             <option value="2023-2024">2023-2024</option>
             <option value="2024-2025">2024-2025</option>
             <option value="2025-2026">2025-2026</option>
@@ -30,86 +50,64 @@
             <option value="2029-2030">2029-2030</option>
         </select>
 
-        <select wire:model="filters.department" class="border focus:outline-none focus:border-blue cursor-pointer rounded p-1 w-[12%] ml-6" placeholder="Department">
+        <select wire:model="filters.department"
+            class="border focus:outline-none focus:border-blue cursor-pointer rounded p-1 w-[17%]"
+            placeholder="Department">
             <option value="">Department(All)</option>
             @foreach ($departments as $department)
                 <option value="{{ $department->department_code }}">{{ $department->department_code }}</option>
             @endforeach
         </select>
-        <button wire:click="applyFilters" class="bg-blue5 hover:bg-blue focus:outline-none focus:border-blue cursor-pointer rounded text-white p-[4px] -mb-3">Apply Filters</button>
+
+        <button wire:click="applyFilters"
+            class="bg-blue5 hover:bg-blue focus:outline-none focus:border-blue cursor-pointer rounded text-white p-[4px] px-4">
+            Apply Filters
+        </button>
     </div>
 
-    <table class=" w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <!-- Table content-->
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="rounded text-xs text-white uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" class="bg-blue5 rounded-tl px-6 py-3">
-                    Course Code
-                </th>
-                <th scope="col" class="bg-blue5 px-6 py-3">
-                    Course Title
-                </th>
-                <th scope="col" class="bg-blue5 px-6 py-3">
-                    School Year
-                </th>
-                <th scope="col" class="bg-blue5 px-6 py-3">
-                    Semester
-                </th>
-                <th scope="col" class="bg-blue5 px-6 py-3">
-                    Term
-                </th>
-                <th scope="col" class="bg-blue5 px-6 py-3">
-                    Submitted At
-                </th>
-                <th scope="col" class="bg-blue5 px-6 py-3">
-                    Version
-                </th>
-                <th scope="col" class="bg-blue5 px-6 py-3">
-                    Status
-                </th>
-                <th scope="col" class="bg-blue5 rounded-tr px-6 py-3">
-                    Action
-                </th>
+                <th scope="col" class="bg-blue5 rounded-tl px-6 py-3">Course Code</th>
+                <th scope="col" class="bg-blue5 px-6 py-3">Course Title</th>
+                <th scope="col" class="bg-blue5 px-6 py-3">School Year</th>
+                <th scope="col" class="bg-blue5 px-6 py-3">Semester</th>
+                <th scope="col" class="bg-blue5 px-6 py-3">Term</th>
+                <th scope="col" class="bg-blue5 px-6 py-3">Submitted At</th>
+                <th scope="col" class="bg-blue5 px-6 py-3">Version</th>
+                <th scope="col" class="bg-blue5 px-6 py-3">Status</th>
+                <th scope="col" class="bg-blue5 rounded-tr px-6 py-3">Action</th>
             </tr>
         </thead>
         <tbody>
-        @foreach ($toss as $tos)
-            <tr class="{{ $loop->iteration % 2 == 0 ? 'bg-white' : 'bg-[#e9edf7]' }} bg-white border- dark:bg-gray-800 dark:border-gray-700 hover:bg-gray4 dark:hover:bg-gray-600">
-                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {{ $tos->course_title }}
-                </td>
-                <td class="px-6 py-4">
-                {{ $tos->course_code }}
-                </td>
-                <td class="px-6 py-4">
-                {{ $tos->bg_school_year }}
-                </td>
-                <td class="px-6 py-4">
-                {{ $tos->course_semester }}
-                </td>
-                <td class="px-6 py-4">
-                {{$tos->tos_term}}
-                </td>
-                <td class="px-6 py-4">
-                {{$tos->chair_submitted_at}}
-                </td>
-                <td class="px-6 py-4">
-                Version {{$tos->tos_version}}
-                </td>
-                <td class="px-6 py-4">
-                <button class="
-                    {{ $tos->status === 'Approved by Chair' ? 'w-[80%]  bg-emerald-200 text-emerald-600 border-2 border-emerald-400 rounded-lg' : '' }}">
-                        {{$tos->tos_status}}
-                    </button>
-                </td>
-                <td class="px-6 py-4 flex">
-                    <form class="" action="" method="GET">
-                        @csrf
-                        <div class="p-4">
-                            <button class="hover:text-yellow hover:underline" type="submit">View</button>
-                        </div>
-                    </form>
-                </td>
-            </tr>
+            @foreach ($toss as $tos)
+                <tr
+                    class="{{ $loop->iteration % 2 == 0 ? 'bg-white' : 'bg-[#e9edf7]' }} bg-white border- dark:bg-gray-800 dark:border-gray-700 hover:bg-gray4 dark:hover:bg-gray-600">
+                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{ $tos->course_title }}
+                    </td>
+                    <td class="px-6 py-4">{{ $tos->course_code }}</td>
+                    <td class="px-6 py-4">{{ $tos->bg_school_year }}</td>
+                    <td class="px-6 py-4">{{ $tos->course_semester }}</td>
+                    <td class="px-6 py-4">{{ $tos->tos_term }}</td>
+                    <td class="px-6 py-4">{{ $tos->chair_submitted_at }}</td>
+                    <td class="px-6 py-4">Version {{ $tos->tos_version }}</td>
+                    <td class="px-6 py-4">
+                        <button class="
+                            {{ $tos->status === 'Approved by Chair' ? 'w-[80%] bg-emerald-200 text-emerald-600 border-2 border-emerald-400 rounded-lg' : '' }}">
+                            {{ $tos->tos_status }}
+                        </button>
+                    </td>
+                    <td class="px-6 py-4 flex">
+                        <form action="" method="GET">
+                            @csrf
+                            <div class="p-4">
+                                <button class="hover:text-yellow hover:underline" type="submit">View</button>
+                            </div>
+                        </form>
+                    </td>
+                </tr>
             @endforeach
         </tbody>
     </table>
