a0ffca2f28057db3bc15c71d7b4af784aeb398ae
Juan Carlos <160289942+ValdeviesoJuan@users.noreply.github.com>
2025-07-18 19:20:10 +0800

    changed_status_highlight
    tos and syllabus statuses in lists

--- resources/views/livewire/admin-syllabus-table.blade.php
+++ resources/views/livewire/admin-syllabus-table.blade.php
@@ -107,14 +107,14 @@
                 <td>{{$syllabus->dean_approved_at}}</td>
                 <td>Version {{$syllabus->version}}</td>
                 <td class="mr-[200px]">
-                    <button class="
-                        {{ $syllabus->status === 'Pending' ? 'w-[80%] bg-amber-100 text-amber-500 border-2 border-amber-300 rounded-lg' : '' }}
-                        {{ $syllabus->status === 'Approved by Chair' ? 'w-[80%]  bg-emerald-300 text-emerald-600 border-2 border-emerald-500  rounded-lg' : '' }}
-                        {{ $syllabus->status === 'Returned by Chair' ? 'w-[80%] bg-rose-300 text-rose-600 border-2 border-rose-500 rounded-lg' : ' ' }}
-                        {{ $syllabus->status === 'Approved by Dean' ? 'w-[80%]  bg-emerald-200 text-emerald-600 border-2 border-emerald-400 rounded-lg' : '' }}
-                        {{ $syllabus->status === 'Returned by Dean' ? 'w-[80%] bg-rose-300 text-rose-600 border-2 border-rose-500 rounded-lg' : ' ' }}">
+                    <div class="
+                        {{ $syllabus->status === 'Pending' ? 'w-[100%] bg-amber-100 text-amber-500 border-2 border-amber-300 rounded-lg' : '' }}
+                        {{ $syllabus->status === 'Approved by Chair' ? 'w-[100%]  bg-emerald-300 text-emerald-600 border-2 border-emerald-500  rounded-lg' : '' }}
+                        {{ $syllabus->status === 'Returned by Chair' ? 'w-[100%] bg-rose-300 text-rose-600 border-2 border-rose-500 rounded-lg' : ' ' }}
+                        {{ $syllabus->status === 'Approved by Dean' ? 'w-[100%]  bg-emerald-200 text-emerald-600 border-2 border-emerald-400 rounded-lg' : '' }}
+                        {{ $syllabus->status === 'Returned by Dean' ? 'w-[100%] bg-rose-300 text-rose-600 border-2 border-rose-500 rounded-lg' : ' ' }}">
                         {{$syllabus->status}}
-                    </button>
+                    </div>
                 </td>
                 <td>
                     <div>
